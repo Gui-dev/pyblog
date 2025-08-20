973a76e9ac586416c57d2c4cffa9bfbc00cf4459
Gui-dev <adrianon10@gmail.com>
2025-08-19 22:08:04 -0300

    test: it should be able to get a post by id

--- src/tests/test_posts.py
+++ src/tests/test_posts.py
@@ -21,7 +21,25 @@ def test_get_all_posts(client):
 	assert isinstance(response.json(), list)
 	
 
-def should_be_able_to_create_a_new_post(client):
+def test_should_be_able_to_get_post_by_id(client):
+	"""should be able to get a post by id"""
+	token = get_auth_token(client)
+	post_data = {
+		'title': 'Meu titulo de teste de ID',
+		'content': 'Meu conteudo de teste ID'
+	}
+	headers = { 'Authorization': f'Bearer {token}' }
+	create_response = client.post('/posts', json=post_data, headers=headers)
+	post_id = create_response.json()['id']
+	
+	response = client.get(f'/posts/{post_id}')
+	
+	assert response.status_code == 200
+	assert response.json()['title'] == post_data['title']
+
+	
+
+def test_should_be_able_to_create_a_new_post(client):
 	"""Should be able to create a new post with a valid user"""
 	token = get_auth_token(client)
 	post_data = {
